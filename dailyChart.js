8ed4e7f0a53eca8eae5b915954d8945d1e094289
Sathish Rama <rama.sathish@gmail.com>
2020-07-22 13:54:59 -0400

    Draft release
    Loaded states names
    Adjusted titles of charts
    Select the state from dropdown

--- dailyChart.js
+++ dailyChart.js
@@ -1,6 +1,12 @@
 
 //const data2 = d3.csv('https://raw.githubusercontent.com/SathishRama/srama.github.io/master/test_covid.csv')
 function dailyChart() {
+       
+       document.getElementById('charttitle').innerHTML = 'US Daily New Cases Trend'; 
+
+       d3.select("#myChart").selectAll("svg").remove();
+       d3.select("#myChart").append("svg").attr("height",360).attr("width",1400);
+
        const data2 = d3.csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv')
         .then(function(data2){
               
@@ -35,16 +41,16 @@ function dailyChart() {
                 k++;
               });
 
-              console.log("Date Ranges " , d3.min(dateRange) , d3.max(dateRange));
+              console.log("Date Chart => Date Ranges " , d3.min(dateRange) , d3.max(dateRange));
 
               xs = d3.scaleBand().domain(dateRange).range([0,1300]);
               ys = d3.scaleLinear().domain([0,d3.max(newcaseRange)]).range([200,0]);
 
-              d3.select(".myDivDailyChart").select("svg").append("g")
+              d3.select("#myChart").select("svg").append("g")
                 .attr("transform","translate(50,50)")
                 .call(d3.axisLeft(ys));
               
-              d3.select(".myDivDailyChart").select("svg").append("g")
+              d3.select("#myChart").select("svg").append("g")
                 .attr("transform","translate(50,250)")
                 .call(d3.axisBottom(xs)
                        .tickFormat(d3.timeFormat("%m.%d"))
@@ -59,7 +65,7 @@ function dailyChart() {
                   .attr("dy", ".15em")
                   .attr("transform", "rotate(-65)");
 
-              d3.select(".myDivDailyChart").select("svg")
+              d3.select("#myChart").select("svg")
                 .append('g').attr("transform","translate(50,50)")
                 .append("path")                       
                 .datum(newCases)              
@@ -75,9 +81,15 @@ function dailyChart() {
         .catch(function() {
               console.log(Error)
         });
+    return true;
 }
 
 function stateChart() {
+
+        document.getElementById('charttitle').innerHTML = 'US State-wise Total Cases'; 
+
+       d3.select("#myChart").selectAll("svg").remove();
+       d3.select("#myChart").append("svg").attr("height",360).attr("width",1400);
 //const data = d3.csv('https://raw.githubusercontent.com/SathishRama/srama.github.io/master/us-states.csv')
         var marginx = 50;
         var marginy = 100;
@@ -98,14 +110,14 @@ function stateChart() {
                 //console.log(each.key , each.value);
               });                          
               
-              colorScale = d3.scaleLinear().domain([0,d3.max(statecases)]).range(["lightblue","red"]);             
+              colorScale = d3.scaleLinear().domain([0,d3.max(statecases)]).range(["grey","pink"]);             
               xs = d3.scaleBand().domain(statelist).range([0,1300]);
               ys = d3.scaleLinear().domain([0,d3.max(statecases)]).range([200,0]);
 
-              d3.select(".myDivStateChart").select("svg").append("g")
+              d3.select("#myChart").select("svg").append("g")
                 .attr("transform","translate(50,50)")
                 .call(d3.axisLeft(ys));
-              d3.select(".myDivStateChart").select("svg").append("g")
+              d3.select("#myChart").select("svg").append("g")
                 .attr("transform","translate(50,250)")
                 .call(d3.axisBottom(xs))
                 .selectAll("text") 
@@ -114,62 +126,91 @@ function stateChart() {
                   .attr("dy", ".15em")
                   .attr("transform", "rotate(-65)");
 
-              d3.select(".myDivStateChart").select("svg")
+              var tooltip = d3.select("#tooltip")
+              //.attr("transform","translate(50,50)")
+
+              d3.select("#myChart").select("svg")
                 .append('g').attr("transform","translate(50,50)")
                 .selectAll('rect')
                 //.append("path")
                 .data(statecounts)
                 .enter()
                 .append('rect')
-                  .attr('x',function(d,i) {return xs(d.key)})
-                  .attr('y',function(d,i) {return ys(d.value)})
-                  //.attr('y',function(d,i) {return (100)})
-                  .attr('width',xs.bandwidth())
-                  .attr('height',function(d) {return (200 - ys(d.value))})
-                  .attr('fill', function(d) {return (colorScale(d.value))})
+                .on("mouseover", function(d) {   
+                  console.log(d3.event.pageX,d3.event.pageY)
+                  tooltip.style("opacity",1)
+                  //.style("left",d3.mouse(this)[0]+"px")
+                  //.style("top",d3.mouse(this)[1]+"px")
+                  .style("left",(d3.event.pageX)+"px")
+                  .style("top",(d3.event.pageY)+"px")                 
+                  .html(d.key+":"+d.value);
+                  d3.select(this)
+                  .attr('stroke','blue');
+                })
+                .on("mouseout", function(d) {       
+                  d3.select(this)
                   .attr('stroke','black');
+                  tooltip.style("opacity",0);
+                })
+                .attr('x',function(d,i) {return xs(d.key)})
+                .attr('y',function(d,i) {return ys(d.value)})
+                //.attr('y',function(d,i) {return (100)})
+                .attr('width',xs.bandwidth())
+                .attr('height',function(d) {return (200 - ys(d.value))})
+                .attr('fill', function(d) {return (colorScale(d.value))})
+                .attr('stroke','black');
 
         })
         .catch(function() {
               console.log(Error)
-        });         
+        });    
+    return true;     
 }
-function countyChart() {
+function countyChart() {      
 
+       console.log(" Enterred - countyChart ");
+       var selected_state = document.getElementById("state").value;
+
+       document.getElementById('charttitle').innerHTML = selected_state+" : "+'Total cases  by county';
+       
+       d3.select("#myChart").selectAll("svg").remove();
+       d3.select("#myChart").append("svg").attr("height",360).attr("width",1400);
+
+        console.log(" From begining - countyChart ");
         var marginx = 50;
         var marginy = 100;
+        //var default_state = 'Alabama' ;
         
-        //const data = d3.csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv')
-        const data = d3.csv('https://raw.githubusercontent.com/SathishRama/srama.github.io/master/us-counties.csv')
-        //const data3 = d3.csv('test_counties.csv')
-        
-        var default_state = 'Alabama'
-        
-        .then(function(data){            
+        const data = d3.csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv')
+        //const data3 = d3.csv('https://raw.githubusercontent.com/SathishRama/srama.github.io/master/test_counties.csv')        
+        .then(function(data3){    
+
+              data3 =  data3.filter(function (d) {return d.state == selected_state})
+
               var countycounts = d3.nest()
-                  .filter(function(d) {return d.state == default_state})
                   .key(function(d) {return d.county }).sortKeys(d3.ascending)
                   .rollup(function(v) { return d3.max( v, function(d) { return parseInt(d.cases)})})
-                  .entries(data3);           
+                  .entries(data3);                               
+
               var countylist = [] ;
               var countycases = [] ;
               var k = 0 ;
               
-              statecounts.forEach( function(each) {
-                countyist[k] = each.key ;
+              countycounts.forEach( function(each) {
+                countylist[k] = each.key ;
                 countycases[k] = each.value ;
                 k++ ;
                 //console.log(each.key , each.value);
-              });                          
-              
-              colorScale = d3.scaleLinear().domain([0,d3.max(countycases)]).range(["lightblue","red"]);             
+              });    
+
+              colorScale = d3.scaleLinear().domain([0,d3.max(countycases)]).range(["lightblue","orange"]);             
               xs = d3.scaleBand().domain(countylist).range([0,1300]);
               ys = d3.scaleLinear().domain([0,d3.max(countycases)]).range([200,0]);
 
-              d3.select(".myDivStateChart").select("svg").append("g")
+              d3.select("#myChart").select("svg").append("g")
                 .attr("transform","translate(50,50)")
                 .call(d3.axisLeft(ys));
-              d3.select(".myDivStateChart").select("svg").append("g")
+              d3.select("#myChart").select("svg").append("g")
                 .attr("transform","translate(50,250)")
                 .call(d3.axisBottom(xs))
                 .selectAll("text") 
@@ -178,7 +219,7 @@ function countyChart() {
                   .attr("dy", ".15em")
                   .attr("transform", "rotate(-65)");
 
-              d3.select(".myDivStateChart").select("svg")
+              d3.select("#myChart").select("svg")
                 .append('g').attr("transform","translate(50,50)")
                 .selectAll('rect')
                 //.append("path")
@@ -195,6 +236,6 @@ function countyChart() {
 
         })
         .catch(function() {
-              console.log(Error)
+            console.log(Error)
         });         
 }
---
+++ loadStatesList.js
@@ -0,0 +1,21 @@
+console.log(" Loading States")
+
+const data = d3.csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv')
+    //const data3 = d3.csv('https://raw.githubusercontent.com/SathishRama/srama.github.io/master/test_counties.csv')        
+    .then(function(data3){              
+
+        var statenames = d3.nest()
+            .key(function(d) {return d.state }).sortKeys(d3.ascending)
+            .entries(data3);   
+        var newState = [];
+       
+        for (i=0;i < statenames.length ; i++) {
+            newState[i] = document.createElement('option');
+            newState[i].innerHTML = statenames[i].key;  
+            document.getElementById("state").add(newState[i]);
+            console.log("Added state : " ,  statenames[i].key)
+          }                                        
+    })
+    .catch(function() {
+        console.log(Error)
+    })         
